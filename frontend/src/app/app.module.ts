57976315893d5ba05d6921138cd35f31a64f6b25
Wilian <wilian.hw@hotmail.com>
2023-10-01 15:15:02 -0300

    Implementado guard das rotas.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,28 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RegisterComponent } from './pages/register/register.component';
-import { InitialComponent } from './pages/initial/initial.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { RegisterComponent } from "./pages/register/register.component";
+import { InitialComponent } from "./pages/post/post.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    InitialComponent
+    InitialComponent,
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ frontend/src/app/guards/isAuthenticated.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { AuthService } from "../services/auth.service";
+import { tap } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class IsAuthenticatedGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.authService.isLoggedIn.pipe(
+      tap((isLoggedIn) => {
+        if (!isLoggedIn) this.router.navigate(["login"]);
+      })
+    );
+  }
+}
