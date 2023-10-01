669cf285cd6931977c280ac385a439783bb5c43e
Wilian <wilian.hw@hotmail.com>
2023-10-01 15:15:18 -0300

    ajuste de importe

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./pages/login/login.component";
 import { RegisterComponent } from "./pages/register/register.component";
-import { InitialComponent } from "./pages/initial/initial.component";
+import { InitialComponent } from "./pages/post/post.component";
 import { IsAuthenticatedGuard } from "./guards/isAuthenticated.guard";
 
 const routes: Routes = [
