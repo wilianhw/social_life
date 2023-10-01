2fffc27ed2e22fbb58d429dbcc161c213b68a2de
Wilian <wilian.hw@hotmail.com>
2023-10-01 09:51:36 -0300

    Configurado cors.

--- backend/src/main/java/dev/wilian/sociallife/infra/security/WebSecurityConfig.java
+++ backend/src/main/java/dev/wilian/sociallife/infra/security/WebSecurityConfig.java
@@ -29,8 +29,8 @@ public class WebSecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
